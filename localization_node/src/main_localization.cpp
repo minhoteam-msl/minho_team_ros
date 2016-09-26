41c2a9c4f504f817bbec846f76008fbdb36507d4
Pedro Osório <pedroosorio.eeic@gmail.com>
2016-09-26 17:01:37 +0100

    Added support for image_transport compilation. Made a simple image publisher in localization

--- localization_node/src/main_localization.cpp
+++ localization_node/src/main_localization.cpp
@@ -5,15 +5,20 @@
 #include "minho_team_ros/hardwareInfo.h"
 #include "minho_team_ros/robotInfo.h"
 
+/////////////////////// TEST
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+///////////////////////
+
+
 using namespace ros;
+using namespace cv;
 using minho_team_ros::hardwareInfo; //Namespace for hardware information msg - SUBSCRIBING
 using minho_team_ros::robotInfo; //Namespace for vision information msg - PUBLISHING
 //Node specific objects
-
-void temp_callback(const hardwareInfo::ConstPtr& msg)
-{
-   ROS_INFO("Received stuff");
-}
 
 int main(int argc, char **argv)
 {
@@ -27,17 +32,31 @@ int main(int argc, char **argv)
 	//Initialize hardwareInfo publisher
 	ros::Publisher robot_info_pub = localization_node.advertise<robotInfo>("robotInfo", 1000);
 	//Initialize controlInfo subscriber
-	ros::Subscriber hardware_info_sub = localization_node.subscribe("hardwareInfo", 
-	                                                            1000, &temp_callback);
-	                                                            
-	                                                            //&hardware::controlInfoCallback,
-	                                                       //     hd);
+	/*ros::Subscriber hardware_info_sub = localization_node.subscribe("hardwareInfo", 
+	                                                            1000, 
+	                                                            &CLASS::CALLBACK
+	                                                            CLASS);*/
 	ROS_WARN("MinhoTeam localization_node started running on ROS.");
 
 
-	ros::AsyncSpinner spinner(2);
-	spinner.start();
-	return a.exec();
+   /////////////////////// TEST
+   Mat test_image;
+   test_image = imread("/home/pedro/catkin_ws/src/minho_team_ros/localization_node/config/sample.png");
+   ros::Rate loop_rate(2);
+   image_transport::ImageTransport it(localization_node);
+   image_transport::Publisher pub = it.advertise("camera/raw", 1);
+   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", test_image).toImageMsg();
+  
+	while(ros::ok()){
+	   ROS_INFO("Sending Image");
+	   pub.publish(msg);
+	   ros::spinOnce();
+	   loop_rate.sleep();
+	}
+	   ///////////////////////
 	
+	/*ros::AsyncSpinner spinner(2);
+	spinner.start();	*/
+	return a.exec();
 	return 0;
 }
