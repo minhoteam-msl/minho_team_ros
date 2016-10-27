347ac831209513263d6810142489fc158a307971
Pedro Osório <pedroosorio.eeic@gmail.com>
2016-10-27 15:42:22 +0100

    Advertising kicking service in .ino

--- low_level/MinhoTeamHardware/MinhoTeamHardware/MinhoTeamHardware.ino
+++ low_level/MinhoTeamHardware/MinhoTeamHardware/MinhoTeamHardware.ino
@@ -294,6 +294,7 @@ void setup() {
   nh.advertiseService(server_resetIMU);
   nh.advertiseService(server_OmniProps);
   nh.advertiseService(server_IMULinTable);
+  nh.advertiseService(server_Kick);
   
   while (!nh.connected()){
     nh.spinOnce();
