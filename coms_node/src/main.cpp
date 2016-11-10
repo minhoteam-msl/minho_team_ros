4816560f10c91d5813cf67d07a0882e1fea76748
Pedro Osório <pedroosorio.eeic@gmail.com>
2016-11-10 21:10:58 +0000

    Added packet_size variable to interAgentInfo msg. Added mutex to assure thread safety in coms. Added system-timed function to serialize data, missing sending over UDP

--- coms_node/src/main.cpp
+++ coms_node/src/main.cpp
@@ -5,16 +5,25 @@
 #include "minho_team_ros/robotInfo.h"
 #include "minho_team_ros/goalKeeperInfo.h"
 #include "minho_team_ros/interAgentInfo.h"
+#include "minho_team_ros/position.h"
+#include "std_msgs/UInt16.h"
 #include <iostream>
 #include <string.h>
 #include <sstream>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+//defines for signal
+#include <sys/time.h>
+#include <unistd.h>
+#include <signal.h>
 
 using namespace ros;
 using minho_team_ros::hardwareInfo; //Namespace for hardwareInfo msg - SUBSCRIBING
 using minho_team_ros::robotInfo; //Namespace for robotInfo msg - SUBSCRIBING
 using minho_team_ros::goalKeeperInfo; //Namespace for goalKeeperInfo msg - SUBSCRIBING
 using minho_team_ros::interAgentInfo; //Namespace for interAgentInfo msg - SENDING OVER UDP/SUBSCRIBING OVER UDP
-
+using minho_team_ros::position;
 // ###### GLOBAL DATA ######
 // \brief subscriber for hardwareInfo message
 ros::Subscriber hw_sub;
@@ -29,6 +38,8 @@ std::stringstream hw_topic_name;
 std::stringstream robot_topic_name;
 /// \brief variable to build the topic name for goalKeeperInfo message
 std::stringstream gk_topic_name;
+/// \brief variable to build the node name
+std::stringstream node_name;
 
 /// \brief message to store most recent hardwareInfo message received
 /// and to store the most recent goalKeeperInfo or robotInfo received. 
@@ -38,9 +49,14 @@ std::stringstream gk_topic_name;
 /// the same data structures
 interAgentInfo message;
 
+/// \brief shared_array of uint8 (unsigned char) used in message 
+/// deserialization
+boost::shared_array<uint8_t> serialization_buffer;
 // #########################
 
 // ######## THREADS ########
+/// \brief a mutex to avoid multi-thread access to message
+pthread_mutex_t message_mutex = PTHREAD_MUTEX_INITIALIZER;
 // #########################
 
 // ### FUNCTION HEADERS ####
@@ -60,17 +76,18 @@ void goalKeeperInfoCallback(const goalKeeperInfo::ConstPtr &msg);
 /// of bytes.
 /// \param msg - pointer to interAgentInfo object to be serialized
 /// \param packet - uint8_t vector containing the serialized message
-void serializeInterAgentInfo(interAgentInfo *msg, uint8_t *packet);
+/// \param packet_size - size of the generated packet
+void serializeInterAgentInfo(interAgentInfo *msg, uint8_t **packet, uint32_t *packet_size);
 
 /// \brief deserializes a string of bytes into a interAgentInfo message
 /// \param packet - uint8_t vector containing message to deserialize
 /// \param msg - pointer to interAgentInfo destination object
 void deserializeInterAgentInfo(uint8_t *packet, interAgentInfo *msg);
 
-
-boost::shared_array<uint8_t> buffer;
+/// \brief main thread to send robot information update over UDP socket
+/// \param signal - system signal for timing purposes
+static void sendRobotInformationUpdate(int signal);
 // #########################
-
 
 /// \brief this node acts as a bridge between the other agents (robots and base
 /// station) and each robot's ROS. It uses UDP broadcast transmission and sends
@@ -78,9 +95,39 @@ boost::shared_array<uint8_t> buffer;
 
 int main(int argc, char **argv)
 {
-	ROS_WARN("Attempting to start Coms services of coms_node.");
+
+   if(argc>1 && argc!=3) { 
+      ROS_ERROR("Must enter robot id and mode as parameter for the simulated robot.\n \
+                Please use -s for simulation, followed by the robot's ID.");
+      exit(1); 
+   }
+   
+   bool mode_real = true;
+   int robot_id = 0;
+   if(argc==3){
+      robot_id = atoi(argv[2]);
+      if(robot_id<0 || robot_id>6){
+         ROS_ERROR("Must enter robot id correctly. Robot id's range from 1 to 6 and 0 to localhost.");
+         exit(2);     
+      }
+      if(!strcmp(argv[1],"-s")) mode_real = false;
+      else { 
+         ROS_ERROR("Must enter mode correctly. Please use -s for simulation.");
+         exit(3); 
+      }
+   }
+   
+   ROS_WARN("Attempting to start Coms services of coms_node.");
+   node_name << "coms_node";
+   if(mode_real) ROS_INFO("Running Teleop for Robot %d.",robot_id);
+   else { 
+      node_name << robot_id;
+      ROS_INFO("Running Teleop for Robot %d in simulation.",robot_id);
+   }
+
+	
 	//Initialize ROS
-	ros::init(argc, argv, "coms_node",ros::init_options::NoSigintHandler);
+	ros::init(argc, argv, node_name.str().c_str(),ros::init_options::NoSigintHandler);
 	//Request node handler
 	ros::NodeHandle coms_node;
 	
@@ -88,6 +135,12 @@ int main(int argc, char **argv)
 	
 	
 	//Setup ROS
+	if(!mode_real){
+	   hw_topic_name << "/minho_gazebo_robot" << robot_id;
+	   robot_topic_name << "/minho_gazebo_robot" << robot_id;
+	   gk_topic_name << "/minho_gazebo_robot" << robot_id;
+	}
+	
 	hw_topic_name << "/hardwareInfo";
 	robot_topic_name << "/robotInfo";
 	gk_topic_name << "/goalKeeperInfo";
@@ -99,24 +152,23 @@ int main(int argc, char **argv)
 	gk_sub = coms_node.subscribe(gk_topic_name.str().c_str(),
 	                             1,&goalKeeperInfoCallback);
 	//Setup Threads
-	
-	
+	/*--- setup the timer 30ms---*/
+	struct itimerval timer;
+	signal(SIGALRM,sendRobotInformationUpdate);
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 33000;
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 33000;
 	
 	
 	ROS_WARN("MinhoTeam coms_node started running on ROS.");
+	
 	// Run spinner
 	ros::AsyncSpinner spinner(2);
 	spinner.start();
-	
-	
-	// Wait threads to join
-	uint8_t *packet;
-	while(ros::ok()){ 
-	   ROS_INFO("IN: %d",message.is_goalkeeper);
-	   serializeInterAgentInfo(&message,packet);
-	   deserializeInterAgentInfo(packet,&message);
-	}
-	return 0;
+	setitimer (ITIMER_REAL, &timer, NULL);
+	while(ros::ok());
+	pthread_exit(NULL);
 }
 
 // ###### FUNCTIONS ########
@@ -124,8 +176,9 @@ int main(int argc, char **argv)
 /// \param msg - message containing received data of hardwareInfo topic
 void hardwareInfoCallback(const hardwareInfo::ConstPtr &msg)
 {
-   //call mutex
+   pthread_mutex_lock (&message_mutex); //Lock mutex
    message.hardware_info = (*msg);
+   pthread_mutex_unlock (&message_mutex); //Unlock mutex
 }
 
 /// \brief callback function of robotInfo messages/topic of ROS
@@ -134,9 +187,10 @@ void hardwareInfoCallback(const hardwareInfo::ConstPtr &msg)
 /// \param msg - message containing received data of robotInfo topic
 void robotInfoCallback(const robotInfo::ConstPtr &msg)
 {
-   //call mutex
+   pthread_mutex_lock (&message_mutex); //Lock mutex
    message.agent_info.robot_info = (*msg); 
    message.is_goalkeeper = false;  
+   pthread_mutex_unlock (&message_mutex); //Unlock mutex
 }
 
 /// \brief callback function of goalKeeperInfo messages/topic of ROS
@@ -145,23 +199,27 @@ void robotInfoCallback(const robotInfo::ConstPtr &msg)
 /// \param msg - message containing received data of goalKeeperInfo topic
 void goalKeeperInfoCallback(const goalKeeperInfo::ConstPtr &msg)
 {
-   //call mutex
+   pthread_mutex_lock (&message_mutex); //Lock mutex
    message.agent_info = (*msg);  
    message.is_goalkeeper = true;  
+   pthread_mutex_unlock (&message_mutex); //Unlock mutex
 }
 
 /// \brief serializes an interAgentInfo message to send over UDP as a string
 /// of bytes.
 /// \param msg - pointer to interAgentInfo object to be serialized
 /// \param packet - uint8_t vector containing the serialized message
-void serializeInterAgentInfo(interAgentInfo *msg, uint8_t *packet)
+void serializeInterAgentInfo(interAgentInfo *msg, uint8_t **packet,  uint32_t *packet_size)
 {  
-   msg->is_goalkeeper = true;
+   pthread_mutex_lock (&message_mutex); //Lock mutex
    uint32_t serial_size = ros::serialization::serializationLength( *msg );
-   buffer.reset(new uint8_t[serial_size]);
-   ros::serialization::OStream stream( buffer.get(), serial_size );
-   ros::serialization::serialize( stream, *msg );
-   packet = buffer.get();
+   serialization_buffer.reset(new uint8_t[serial_size]);
+   msg->packet_size = serial_size; 
+   (*packet_size) = serial_size;
+   ros::serialization::OStream stream( serialization_buffer.get(), serial_size );
+   ros::serialization::serialize( stream, *msg);
+   (*packet) = serialization_buffer.get();
+	pthread_mutex_unlock (&message_mutex); //Unlock mutex
 }
 
 /// \brief deserializes a string of bytes into a interAgentInfo message
@@ -169,12 +227,23 @@ void serializeInterAgentInfo(interAgentInfo *msg, uint8_t *packet)
 /// \param msg - pointer to interAgentInfo destination object
 void deserializeInterAgentInfo(uint8_t *packet, interAgentInfo *msg)
 {  
-   interAgentInfo temp;
-   uint32_t serial_size = 105;
-   ros::serialization::IStream i_stream(packet, serial_size);
-   ros::serialization::deserialize(i_stream, temp);
-   
-   ROS_INFO("OUT: %d",temp.is_goalkeeper);
-   msg->is_goalkeeper = false;
+   std_msgs::UInt16 packet_size;
+   ros::serialization::IStream psize_stream(packet, 2);
+   ros::serialization::deserialize(psize_stream, packet_size);
+   ros::serialization::IStream istream(packet, packet_size.data);
+   ros::serialization::deserialize(istream, *msg);
+}
+
+/// \brief main thread to send robot information update over UDP socket
+/// \param signal - system signal for timing purposes
+static void sendRobotInformationUpdate(int signal)
+{
+   if(signal==SIGALRM){
+      uint8_t *packet;
+      uint32_t packet_size;
+      serializeInterAgentInfo(&message,&packet,&packet_size);
+      // Send packet of size packet_size through UDP
+
+	}
 }
 // #########################
