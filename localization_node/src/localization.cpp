18a0e50598674abd0be1d1b423e9a98ba6435c5f
Pedro Osório <pedroosorio.eeic@gmail.com>
2017-01-13 12:12:05 +0000

    Added prints for wheel velocity

--- localization_node/src/localization.cpp
+++ localization_node/src/localization.cpp
@@ -309,6 +309,9 @@ void Localization::hardwareCallback(const hardwareInfo::ConstPtr &msg)
    v  = (-v1+v3)/(2*SINT);
    vn = (v1-2*v2+v3)/(2*(COST+1));
    w  = (v1+2*COST*v2+v3)/(2*DROB*(COST+1));
+
+   ROS_INFO("VelW: %.2f | %.2f | %.2f \n Vel: %.2f | %.2f | %.2f",
+   v1, v2, v3, v, vn, w);           
    halfTeta = normalizeAngleRad(w*DELTA_T_2)-msg->imu_value*DEGTORAD;
    
    // Acumulate odometry estimate while not used by other functions
