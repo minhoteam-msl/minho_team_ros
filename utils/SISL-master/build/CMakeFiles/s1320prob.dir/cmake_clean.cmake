c29dc8cdb7e3351511174b24c2bc80b5db7d78fc
Pedro Osório <pedroosorio.eeic@gmail.com>
2017-02-16 13:36:43 +0000

    Added lib sisl

---
+++ utils/SISL-master/build/CMakeFiles/s1320prob.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/s1320prob.dir/app/s1320prob.C.o"
+  "s1320prob.pdb"
+  "s1320prob"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/s1320prob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ utils/SISL-master/build/CMakeFiles/s1606prob.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/s1606prob.dir/app/s1606prob.C.o"
+  "s1606prob.pdb"
+  "s1606prob"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/s1606prob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
